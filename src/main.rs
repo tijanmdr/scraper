30bc989e1c9f824ba9a51b75e7410ceb0a191ac1
tijan <tijanmdR@gmail.com>
2024-02-18 21:38:14 +1100

    scraper done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    
+    // target the website
+    let response = reqwest::blocking::get("https://rust-trends.com/newsletter/rust-in-action-10-project-ideas-to-elevate-your-skills/");
+
+    // extract content from the above response
+    let html_content = response.unwrap().text().unwrap();
+    print!("{html_content}");
 }
